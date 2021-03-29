5d1caa2e9a9967f3425b924b7e690965645df65e
Robert O'Callahan <robert@ocallahan.org>
2021-03-29 14:06:58 +1300

    Initial commit

---
+++ submodule.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include "submodule.h"
+
+void submodule_function(void) {
+  puts("Submodule");
+}
---
+++ submodule.h
@@ -0,0 +1,6 @@
+#ifndef SUBMODULE_H
+#define SUBMODULE_H
+
+void submodule_function(void);
+
+#endif
